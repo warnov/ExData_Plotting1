52b0115b81890fb6801eb966e839e816caa8bd08
warnov <warnov@gmail.com>
2015-02-05 23:31:05 -0500

    strptime used in prepareData.R. .gitignore file added

--- plot1.R
+++ plot1.R
@@ -1,26 +1,8 @@
 plot1 <- function(allHouseHold){
+  source('prepareData.R')
 
-  #We download the zip file
-  print("Downloading Data...")
-  temp <- tempfile()
-  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)  
-  
-  #Loading the file data into memory
-  print("Getting data into memory...")
-  allHouseHold <- read.csv(unz(temp, "household_power_consumption.txt"),
-                           sep = ";",na.strings = "?")  
-  #Deleting temp files
-  unlink(temp)
-  
-  #Processing data
-  print("Processing data and generating graphics")
-  #Casting the values from factor to Data types
-  allHouseHold$Date <- as.Date(allHouseHold$Date, "%d/%m/%Y")
-  #Filtering the data
-  beginning <- as.Date("2007-02-01")
-  ending <- as.Date("2007-02-02")
-  workingData <- allHouseHold[(allHouseHold$Date>=beginning &
-                                 allHouseHold$Date<=ending),]
+  #Let's get the data ready to work with
+  workingData <- prepareData()
   
   #Preparing the device with the specified size
   png(
